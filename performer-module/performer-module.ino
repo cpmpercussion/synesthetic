0064643754729257c911ec3504470b57712daa11
Charles Martin <cpm@charlesmartin.com.au>
2017-06-25 23:17:25 +0200

    Two strings of pixels and one centre

--- performer-module/performer-module.ino
+++ performer-module/performer-module.ino
@@ -13,10 +13,10 @@
 
 // Example of zeroconf service announcement:
 // https://github.com/esp8266/Arduino/blob/master/libraries/ESP8266mDNS/examples/mDNS-SD_Extended/mDNS-SD_Extended.ino
+// 192.168.1.9
 
-
-char ssid[] = "deepthought";          // your network SSID (name)
-char pass[] = "zarniwoop";                    // your network password
+char ssid[] = "NETGEAR50";          // your network SSID (name)
+char pass[] = "zanyocean960";                    // your network password
 
 // A UDP instance to let us send and receive packets over UDP
 WiFiUDP Udp;
@@ -24,7 +24,10 @@ const IPAddress outIp(10, 40, 10, 105);     // remote IP (not needed for receive
 char hostString[16] = {0};
 const unsigned int localPort = 8888;        // local port to listen for UDP packets (here's where we send the packets)
 
-#define NEOPIN D4 // USE D4 on d1minilite
+#define LEFT_PIXELS D3
+#define RIGHT_PIXELS D4
+#define CENTRE_PIXEL D2
+#define NUM_PIXELS_PER_STRING 4
 
 // Parameter 1 = number of pixels in strip
 // Parameter 2 = pin number (most are valid)
@@ -33,7 +36,9 @@ const unsigned int localPort = 8888;        // local port to listen for UDP pack
 //   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products) - tick
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(1, NEOPIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel left_string = Adafruit_NeoPixel(4, LEFT_PIXELS, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel right_string = Adafruit_NeoPixel(4, RIGHT_PIXELS, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel centre = Adafruit_NeoPixel(1, CENTRE_PIXEL, NEO_GRB + NEO_KHZ800);
 
 OSCErrorCode error;
 unsigned int ledState = LOW;              // LOW means led is *on*
@@ -41,10 +46,32 @@ unsigned int ledState = LOW;              // LOW means led is *on*
 void setup() {
   pinMode(BUILTIN_LED, OUTPUT);
   digitalWrite(BUILTIN_LED, ledState);    // turn *on* led
-  pixels.begin(); // This initializes the NeoPixel library.
-  pixels.setBrightness(25);
-  pixels.setPixelColor(0, 100, 0, 100);
-  pixels.show();
+  left_string.begin(); // This initializes the NeoPixel library.
+  right_string.begin();
+  centre.begin();
+
+  left_string.setBrightness(25);
+  right_string.setBrightness(25);
+  centre.setBrightness(25);
+
+  left_string.setPixelColor(0, 100, 0, 100);
+  right_string.setPixelColor(0, 100, 0, 100);
+
+  left_string.setPixelColor(1, 100, 100,0);
+  right_string.setPixelColor(1, 100, 100,0);
+  
+  left_string.setPixelColor(2, 0, 100, 100);
+  right_string.setPixelColor(2, 0, 100, 100);
+  
+  left_string.setPixelColor(3, 100, 50, 75);
+  right_string.setPixelColor(3, 100, 50, 75);
+
+  centre.setPixelColor(0,100,50,0);
+
+  left_string.show();
+  right_string.show();
+  centre.show();
+
   Serial.begin(115200);
   delay(100);
 
@@ -91,10 +118,22 @@ void pixel(OSCMessage &msg) {
   int r = msg.getInt(0);
   int g = msg.getInt(1);
   int b = msg.getInt(2);
-  pixels.setBrightness(msg.getInt(3));
-  pixels.setPixelColor(0, r, g, b); // Moderately bright green color.
-  pixels.show(); // This sends the updated pixel color to the hardware.
-  Serial.printf("/pixel: %d %d %d\n", r,g, b);
+
+  left_string.setBrightness(msg.getInt(3));
+  right_string.setBrightness(msg.getInt(3));
+  centre.setBrightness(msg.getInt(3));
+
+  for (int i = 0; i < NUM_PIXELS_PER_STRING; i++) {
+    left_string.setPixelColor(i, r, g, b);
+    right_string.setPixelColor(i, r, g, b);
+  }
+  centre.setPixelColor(0, r, g, b);
+
+  left_string.show();
+  right_string.show();
+  centre.show();
+
+  // Serial.printf("/pixel: %d %d %d\n", r,g, b);
 }
 
 void loop() {
